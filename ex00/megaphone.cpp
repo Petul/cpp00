d19d7246caeced8dadf644b5f3eb73f2ab064f02
Peter <leander.peter@gmail.com>
2024-11-14 15:34:46 +0200

    Write boilerplate

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Megahpone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pleander <pleander@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 15:27:11 by pleander          #+#    #+#             */
+/*   Updated: 2024/11/14 15:33:57 by pleander         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	std::string	text { "hello world"};
+	std::cout << text << std::endl;
+}
