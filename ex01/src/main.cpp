846d15ca590ba1586b446dc391f7ee0a0143b5dd
Peter <leander.peter@gmail.com>
2024-11-17 13:25:53 +0200

    Fix input reading

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,31 +6,28 @@
 /*   By: pleander <pleander@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 16:41:05 by pleander          #+#    #+#             */
-/*   Updated: 2024/11/16 13:25:35 by pleander         ###   ########.fr       */
+/*   Updated: 2024/11/17 10:23:29 by pleander         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.h"
 #include <iostream>
 
+std::string get_input(std::string msg) {
+	std::string input;
+
+	if (msg != "")
+		std::cout << msg << ": " << std::endl;
+	std::getline(std::cin, input);
+	return (input);
+}
+
 void add_contact(PhoneBook &pb) {
-	std::string first_name;
-	std::string last_name;
-	std::string nickname;
-	std::string phone_number;
-	std::string darkest_secret;
-
-	std::cout << "First name: " << std::endl;
-	std::cin >> first_name;
-	std::cout << "Last name: " << std::endl;
-	std::cin >> last_name;
-	std::cout << "Nickname: " << std::endl;
-	std::cin >> nickname;
-	std::cout << "Phone number: " << std::endl;
-	std::cin >> phone_number;
-	std::cout << "Darkest secret: " << std::endl;
-	std::cin >> darkest_secret;
-
+	std::string first_name = get_input("First name");
+	std::string last_name = get_input("Last name");
+	std::string nickname = get_input("Nickname");
+	std::string phone_number = get_input("Phone number");
+	std::string darkest_secret = get_input("Darkest secret");
 	pb.add_contact(first_name, last_name, nickname, phone_number,
 	               darkest_secret);
 	return;
@@ -41,8 +38,7 @@ void search(PhoneBook &pb) {
 	std::string id;
 
 	pb.show_all();
-	std::cout << "Enter index to show contact: " << std::endl;
-	std::cin >> id;
+	id = get_input("Enter index to show contact");
 	try {
 		pb.show_contact((size_t)stoi(id));
 	}
@@ -56,11 +52,10 @@ int main(void) {
 	PhoneBook pb;
 	std::string option;
 	while (option != "EXIT") {
-		std::cout << "\nEnter ADD, SEARCH or EXIT:" << std::endl;
-		std::cin >> option;
 		if (std::cin.eof()) {
 			return 0;
 		}
+		option = get_input("\nEnter ADD, SEARCH or EXIT");
 		if (option == "ADD") {
 			add_contact(pb);
 		} else if (option == "SEARCH") {
